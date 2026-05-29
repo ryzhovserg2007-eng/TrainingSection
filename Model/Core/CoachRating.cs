11956cd8d2cbff8e249b6507f0a6e22622464501
ryzhovserg2007-eng <ryzhov.serg.2007@gmail.com>
2026-05-29 22:06:44 +0300

    Add CoachRating class

---
+++ Model/Core/CoachRating.cs
@@ -0,0 +1,10 @@
+namespace Model.Core;
+
+/// <summary>
+/// Класс для хранения оценки от спортсмена
+/// </summary>
+public class CoachRating
+{
+    public int LessonsCount { get; set; }
+    public double Rating { get; set; }
+}
